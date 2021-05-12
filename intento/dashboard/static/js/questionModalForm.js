b6256e938d655213c062ecb709be6552df551fe6
Andre Perez <andremztm@gmail.com>
2021-05-12 15:21:24 -0300

    Modal now displays the Preview as a question

--- intento/dashboard/static/js/questionModalForm.js
+++ intento/dashboard/static/js/questionModalForm.js
@@ -1,16 +1,16 @@
-const alternatives = ['A', 'B', 'C', 'D', 'E'];
+const alternatives = {'A': '&#9398;', 'B': '&#9399', 'C': '&#9400', 'D': '&#9401', 'E': '&#9402'};
+const modalBefore = $("#exampleModal").html(); // save modal HTML before modifications
 
 $(".previewModal").click(function () {
-  let modalBefore = $("#exampleModal").html(); // save modal HTML before modifications
   let questionContent = document.getElementsByClassName('ql-editor'); // gets quilljs input
   for (let i = 0; i < questionContent.length; i++) {
     let paragraph = questionContent.item(i);
     let textfieldId = paragraph.parentElement.id;
-    if (alternatives.indexOf(textfieldId.slice(-1)) == -1) {
+    if (alternatives[textfieldId.slice(-1)]  == undefined) {
       document.getElementById('modal-question').innerHTML += '<p class="modal-text-fields">' + paragraph.innerHTML + '<hr>'
     } else {
-      questionId = textfieldId.slice(-1)
-      formattedQuestion = '<p class="modal-text-fields">' + questionId + ') ' + '</p>' + paragraph.innerHTML
+      let questionId = textfieldId.slice(-1)
+      let formattedQuestion = '<div id="alternatives"><p class="modal-text-fields">' + alternatives[questionId] + '</p> ' + paragraph.innerHTML + '</div>'
       document.getElementById('modal-question').innerHTML += formattedQuestion;
     }
   }
